63a2c9bbb4253641383ed917c274eeb90e065974
Francis.Mensah <francis.mensah@hubtel.com>
2024-11-21 00:01:04 +0000

    feat: add persistence using SQLite database

--- internal/controllers/shorten.go
+++ internal/controllers/shorten.go
@@ -1,42 +1,65 @@
 package controllers
 
 import (
-	"fmt"
+	"database/sql"
+	"github.com/Francismensah/url-shortner-app/internal/db"
 	"github.com/Francismensah/url-shortner-app/internal/url"
 	"html/template"
 	"net/http"
 	"strings"
 )
 
-func Shorten(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Not implemented", http.StatusMethodNotAllowed)
-		return
-	}
+func Shorten(lite *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Not implemented", http.StatusMethodNotAllowed)
+			return
+		}
 
-	originalURL := r.FormValue("url")
-	if originalURL == "" {
-		http.Error(w, "URL not provide", http.StatusBadRequest)
-		return
-	}
-	if !strings.HasPrefix(originalURL, "http://") && !strings.HasPrefix(originalURL, "https://") {
-		originalURL = "https://" + originalURL
-	}
+		originalURL := r.FormValue("url")
+		if originalURL == "" {
+			http.Error(w, "URL not provide", http.StatusBadRequest)
+			return
+		}
+		if !strings.HasPrefix(originalURL, "http://") && !strings.HasPrefix(originalURL, "https://") {
+			originalURL = "https://" + originalURL
+		}
 
-	hash, shortURL := url.Shorten(originalURL)
+		shortURL, _ := url.Shorten(originalURL)
 
-	fmt.Println(hash)
+		if err := db.StoreURL(lite, shortURL, originalURL); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 
-	data := map[string]string{
-		"ShortURL": shortURL,
+		data := map[string]string{
+			"ShortURL": shortURL,
+		}
+
+		t, err := template.ParseFiles("internal/views/shorten.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		if err = t.Execute(w, data); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 	}
-	t, err := template.ParseFiles("internal/views/shorten.html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	if err = t.Execute(w, data); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+}
+
+func Proxy(lite *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		shortUrl := r.URL.Path[:1]
+		if shortUrl == "" {
+			http.Error(w, "URL not provide", http.StatusBadRequest)
+			return
+		}
+		origURL, err := db.GetoriginalURL(lite, shortUrl)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusNotFound)
+			return
+		}
+		http.Redirect(w, r, origURL, http.StatusPermanentRedirect)
 	}
 }
